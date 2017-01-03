c98f9a3ae4a8694f6e276b3666c6680b66959410
zhushan <tonyshanm@163.com>
2017-01-03 17:37:02 +0800

    碰撞

---
+++ Block Wars/Block Wars/cubicCreate.swift
@@ -0,0 +1,27 @@
+//
+//  cubicCreate.swift
+//  Block Wars
+//
+//  Created by 紫贝壳 on 2017/1/3.
+//  Copyright © 2017年 紫贝壳. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+let cubicCatargy:UInt32 = 0x00000001
+
+class cubicCreate: NSObject {
+   class func makeCubic() -> SKSpriteNode {
+        let cubic:SKSpriteNode = SKSpriteNode.init()
+        cubic.size = CGSize(width: 40, height: 40)
+        cubic.color = UIColor.yellow
+        cubic.name = "cubic"
+        cubic.physicsBody = SKPhysicsBody.init(rectangleOf: cubic.size)
+        cubic.physicsBody?.categoryBitMask = cubicCatargy
+        cubic.physicsBody?.contactTestBitMask = cubicCatargy
+        cubic.physicsBody?.collisionBitMask = cubicCatargy
+        cubic.physicsBody?.affectedByGravity = false
+        cubic.physicsBody?.usesPreciseCollisionDetection = true
+        return cubic
+    }
+}
--- Block Wars/Block Wars/mainScene.swift
+++ Block Wars/Block Wars/mainScene.swift
@@ -14,23 +14,30 @@ let hscreen = UIScreen.main.bounds.size.height
 let hbtn:CGFloat = 50.0/667.0 * hscreen
 let margin:CGFloat = 20.0/375.0 * wscreen
 let wbtn:CGFloat = (wscreen - margin * 5)/4.0
+var haveFinish:Bool = false
+var fourCubic:SKSpriteNode? = nil
+var selfcenterView:SKView? = nil
+var cubicX:CGFloat = wscreen * 0.5
 
-class mainScene: SKScene {
+
+
+class mainScene: SKScene,SKPhysicsContactDelegate {
 
     override func didMove(to view: SKView) {
         createContent()
+        self.physicsWorld.contactDelegate = self;
     }
     func createContent() {
-        let centerView:SKView = SKView.init()
-        centerView.backgroundColor = UIColor.gray
-        self.view?.addSubview(centerView)
-        centerView.snp.makeConstraints { (make) in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview().offset(-20.0/667.0 * hscreen)
-            make.width.equalTo(wscreen - 20.0/375.0 * wscreen)
-            make.height.equalTo(hscreen - 120.0/667.0 * hscreen)
-        }
-        
+//        let centerView:SKView = SKView.init()
+//        centerView.backgroundColor = UIColor.gray
+//        self.view?.addSubview(centerView)
+//        selfcenterView = centerView
+//        centerView.snp.makeConstraints { (make) in
+//            make.centerX.equalToSuperview()
+//            make.top.equalToSuperview().offset(64.0/667.0 * hscreen)
+//            make.width.equalTo(wscreen - 20.0/375.0 * wscreen)
+//            make.height.equalTo(hscreen - 140.0/667.0 * hscreen)
+//        }
         
         for index in 1...4 {
             let btn:UIButton = UIButton.init()
@@ -55,9 +62,40 @@ class mainScene: SKScene {
                 
             }
         }
-        
-
+        defultCubic()
+        //kaishi
+        addCubic()
     }
+    //碰撞代理方法
+    func didBegin(_ contact: SKPhysicsContact) {
+       print("pengle")
+        let nodeA:SKNode = contact.bodyA.node!
+        let nodeB:SKNode = contact.bodyB.node!
+        if nodeA.hasActions() {
+            nodeA.removeAction(forKey: "other")
+        }
+        if nodeB.hasActions() {
+            nodeB.removeAction(forKey: "our")
+            addCubic()
+        }
+    }
+    
+    func defultCubic() {
+        let defult:SKSpriteNode = cubicCreate.makeCubic()
+        defult.position = CGPoint(x: wscreen * 0.5, y: 600);
+        self.addChild(defult)
+        
+    }
+    func addCubic() {
+        if haveFinish == false {
+            let cubic:SKSpriteNode = cubicCreate.makeCubic()
+            cubic.position = CGPoint(x: wscreen * 0.5, y: 204.0/667.0 * hscreen)
+            let runaction:SKAction = SKAction.sequence([SKAction.moveBy(x: 0, y: 1500, duration: 50)])
+            cubic.run(runaction, withKey: "our")
+            self.addChild(cubic)
+        }
+    }
+    
     func leftClick() {
         print("zuo")
     }
