b5c031e50450a30947eb0bbdac2ec8925423b2fc
zhushan <tonyshanm@163.com>
2017-01-03 13:31:52 +0800

    底部四个按钮

--- Block Wars/Block Wars/mainScene.swift
+++ Block Wars/Block Wars/mainScene.swift
@@ -8,25 +8,66 @@
 
 import UIKit
 import SpriteKit
-
+import SnapKit
 let wscreen = UIScreen.main.bounds.size.width
 let hscreen = UIScreen.main.bounds.size.height
+let hbtn:CGFloat = 50.0/667.0 * hscreen
+let margin:CGFloat = 20.0/375.0 * wscreen
+let wbtn:CGFloat = (wscreen - margin * 5)/4.0
 
 class mainScene: SKScene {
 
     override func didMove(to view: SKView) {
-//        self.backgroundColor = UIColor.blue
         createContent()
     }
     func createContent() {
-        let centerView:SKView = SKView.init(frame: CGRect(x: 10, y:  100, width: wscreen - 20, height: hscreen - 200))
+        let centerView:SKView = SKView.init()
         centerView.backgroundColor = UIColor.gray
         self.view?.addSubview(centerView)
+        centerView.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-20.0/667.0 * hscreen)
+            make.width.equalTo(wscreen - 20.0/375.0 * wscreen)
+            make.height.equalTo(hscreen - 120.0/667.0 * hscreen)
+        }
         
-        let fireBtn:UIButton = UIButton.init(type: .custom)
-        fireBtn.frame = CGRect(x: wscreen - 50, y: hscreen - 100, width: 50, height: 50)
-        fireBtn.backgroundColor = UIColor.blue
-        self.view?.addSubview(fireBtn)
         
+        for index in 1...4 {
+            let btn:UIButton = UIButton.init()
+            btn.backgroundColor = UIColor.green
+            self.view?.addSubview(btn)
+            btn.snp.makeConstraints({ (make) in
+                make.left.equalTo(self.view!).offset(margin * CGFloat(index)  + wbtn * CGFloat(index - 1))
+                make.bottom.equalTo(self.view!).offset(margin * -1)
+                make.height.equalTo(hbtn)
+                make.width.equalTo(wbtn)
+            })
+            switch index {
+            case 1:
+                btn.addTarget(self, action: #selector(mainScene.rightClick), for: .touchUpInside)
+            case 2:
+                btn.addTarget(self, action: #selector(mainScene.leftClick), for: .touchUpInside)
+            case 3:
+                btn.addTarget(self, action: #selector(mainScene.fireClick), for: .touchUpInside)
+            case 4:
+                btn.addTarget(self, action: #selector(mainScene.changeClick), for: .touchUpInside)
+            default: break
+                
+            }
+        }
+        
+
+    }
+    func leftClick() {
+        print("zuo")
+    }
+    func rightClick() {
+        print("you")
+    }
+    func fireClick() {
+        print("jiang")
+    }
+    func changeClick() {
+       print("bian")
     }
 }
